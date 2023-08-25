b567e3b45a2e6f21d9397ee148583871e1a53eb5
Gayan Samuditha <gayansamudithastack@gmail.com>
2023-08-25 15:35:44 -0400

    Original code from OHDSI

---
+++ src/main/java/org/ohdsi/webapi/vocabulary/VocabularySearchServiceImpl.java
@@ -0,0 +1,30 @@
+package org.ohdsi.webapi.vocabulary;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VocabularySearchServiceImpl implements VocabularySearchService {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+    
+    private static final String NO_PROVIDER_ERROR = "There is no vocabulary search provider which for sourceKey: %s";
+
+    private final SearchProvider[] searchProviders;
+
+    public VocabularySearchServiceImpl(SearchProvider[] searchProviders) {
+        this.searchProviders = searchProviders;
+    }
+
+    @Override
+    public SearchProvider getSearchProvider(SearchProviderConfig config) {
+        return Arrays.stream(searchProviders)
+                .sorted(Comparator.comparingInt(SearchProvider::getPriority))
+                .filter(p -> p.supports(config.getVersionKey()))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException(String.format(NO_PROVIDER_ERROR, config.getSourceKey())));
+    }
+}
